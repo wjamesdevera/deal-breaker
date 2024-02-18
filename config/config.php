05c5b7f37054869c58366ab5d75c4ada63c8ef4a
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-02-18 23:47:31 +0800

    created Database class
    created a database class for easier connection with the database

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_SOCKET', '');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'deal_breaker_db');
---
+++ src/model/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DealBreaker\Model;
+
+require_once dirname(__DIR__) ."\\config\\config.php";
+
+use PDOException;
+use PDO;
+
+class Database
+{
+    private $host = DB_HOST;
+    private $port = DB_PORT;
+    private $username = DB_USERNAME;
+    private $password = DB_PASSWORD;
+    private $databaseName = DB_NAME;
+
+    protected function connect(): PDO
+    {
+        try {
+            $dbh = new PDO("mysql:host={$this->host};port={$this->port};dbname={$this->databaseName}", $this->username, $this->password);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $dbh;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+}
