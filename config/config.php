c6018cbab6753729078d4a0820ac7b55cac80e31
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-02-19 00:27:14 +0800

    updated config file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_SOCKET', '');
+// Place our database username here
+define('DB_USERNAME', 'root');
+// Place our database password here
+define('DB_PASSWORD', '');
+// Place our database name here
+define('DB_NAME', 'deal_breaker_db');
