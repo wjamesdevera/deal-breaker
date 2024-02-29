20b5750999ad30be7a0972e8bd265edb6c96eb17
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-02-29 22:27:36 +0800

    added random background image functionality

--- public/play.php
+++ public/play.php
@@ -20,6 +20,10 @@ if (isset($_SESSION["game"])) {
   $game = $_SESSION['game'];
 }
 
+if (!isset($_SESSION['background-img'])) {
+  $_SESSION['background-img'] = random_int(1, 11) . ".jpg";
+}
+
 include_once './includes/header.php';
 ?>
 <main class="d-flex justify-content-center align-items-center flex-fill">
@@ -28,8 +32,21 @@ include_once './includes/header.php';
     require_once './includes/game.php';
   ?>
   <?php else :
+    unset($_SESSION['background-img']);
     header('location: index.php');
     die();
   endif ?>
 </main>
+<style>
+  main {
+    background-image: 
+    url('./img/bg/<?= $_SESSION['background-img'] ?>');
+    background-color: rgba(0, 0, 0, 0.50);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: center;
+    background-blend-mode:color-burn;
+  }
+</style>
 <?php require_once './includes/footer.php' ?>
