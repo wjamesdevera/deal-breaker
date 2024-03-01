b08c6baab59cbfb34fa31be949c13cc5290b8dc2
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-03-01 17:30:46 +0800

    added render Win and render Lose

--- src/views/GameView.php
+++ src/views/GameView.php
@@ -5,4 +5,17 @@ namespace DealBreaker\Views;
 class GameView
 {
 
+    private static function renderWin()
+    {
+        return '<div class="you-won-message fs-2 mx-auto flex-fill puff-in-center">
+                YOU WON!
+                </div>';
+    }
+
+    private static function renderLose()
+    {
+        return '<div class="you-won-message fs-2 mx-auto flex-fill puff-in-center">
+                YOU LOSE!
+                </div>';
+    }
 }
