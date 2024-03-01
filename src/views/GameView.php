ced54df91dc4a62ccbda985880a844417b21da21
Winfrey De Vera <w.jamesdevera@gmail.com>
2024-03-01 17:36:10 +0800

    change from return type to echo
    change echo since the function name means to render not return a string

--- src/views/GameView.php
+++ src/views/GameView.php
@@ -7,15 +7,16 @@ class GameView
 
     private static function renderWin()
     {
-        return '<div class="you-won-message fs-2 mx-auto flex-fill puff-in-center">
+        echo '<div class="you-won-message fs-2 mx-auto flex-fill puff-in-center">
                 YOU WON!
                 </div>';
     }
 
     private static function renderLose()
     {
-        return '<div class="you-won-message fs-2 mx-auto flex-fill puff-in-center">
+        echo '<div class="you-won-message fs-2 mx-auto flex-fill puff-in-center">
                 YOU LOSE!
                 </div>';
     }
+
 }
